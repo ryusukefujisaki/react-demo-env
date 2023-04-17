435cc20a410cb0996e92a15a39e2de059ad8b04d
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-17 12:11:06 +0900

    Add create-react-app container

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 set -eux
 
 git clone git@github.com:ryusukefujisaki/react-demo-frontend-app.git frontend-app
+git clone git@github.com:ryusukefujisaki/react-demo-create-react-app.git create-react-app
